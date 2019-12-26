d74e194a4828b7d73f81d768502054c7d65a2f81
sesankmallikarjuna <sesank.mallikarjuna@gmail.com>
2019-12-26 10:16:58 -0600

    Fixed sentiment and subjectivity storing problem

--- scrape.py
+++ scrape.py
@@ -8,6 +8,8 @@ import pytz
 import time as time_right_now
 import sentiment
 import manage_data
+import alpaca_trade_api as trade_api
+import time as t
 
 stocks_list = ['TSLA', 'GOOGL', 'AAPL', 'MSFT', 'BABA']
 stock_dict = {}
@@ -19,42 +21,50 @@ base_url = 'https://finance.yahoo.com/quote/'
 rest_of_url = '?p='
 
 def scrape(stocks = stocks_list, dictionary = stock_dict):
-    for ticker in stocks_list:
-        url = base_url + ticker + rest_of_url + ticker
-        r = requests.get(url)
-        soup = bs4.BeautifulSoup(r.text, 'lxml')
-        print(ticker + ": " + url)
+    key_id = "PKBOHP3XJ2CGC13FQ9Y0"
+    secret_key = "d0O/vHoP2yQSzEBpszuVEplLMypcPLDmKe77KNTL"
 
-        key_stats_url = base_url + ticker + '/key-statistics' + rest_of_url + ticker
-        key_stats_req = requests.get(key_stats_url)
-        stats_soup = bs4.BeautifulSoup(key_stats_req.text, 'lxml')
+    api = trade_api.REST(key_id, secret_key)
+    clock = api.get_clock()
 
-        # date/time
-        now = datetime.datetime.now(pytz.timezone('US/Central'))
-        date = now.strftime("%b %d, %Y")
-        time = now.strftime("%H:%M")
+    while(clock.is_open):
+        for ticker in stocks_list:
+            url = base_url + ticker + rest_of_url + ticker
+            r = requests.get(url)
+            soup = bs4.BeautifulSoup(r.text, 'lxml')
+            print(ticker + ": " + url)
 
-        # price
-        price = float( soup.find("div", {'class':"My(6px) Pos(r) smartphone_Mt(6px)"}).find('span').text.replace(',', '') )
+            key_stats_url = base_url + ticker + '/key-statistics' + rest_of_url + ticker
+            key_stats_req = requests.get(key_stats_url)
+            stats_soup = bs4.BeautifulSoup(key_stats_req.text, 'lxml')
 
-        # Moving Averages
-        fifty_day_SMA = float( stats_soup.find_all("tr" , {"class":"Bxz(bb) H(36px) BdB Bdbc($seperatorColor)"})[4].find("td", {"class": "Fz(s) Fw(500) Ta(end) Pstart(10px) Miw(60px)"}).text.replace(",", '') )
-        t_hundred_day_SMA = float( stats_soup.find_all("tr" , {"class":"Bxz(bb) H(36px) BdB Bdbc($seperatorColor)"})[5].find("td", {"class": "Fz(s) Fw(500) Ta(end) Pstart(10px) Miw(60px)"}).text.replace(",", '') )
+            # date/time
+            now = datetime.datetime.now(pytz.timezone('US/Central'))
+            date = now.strftime("%b %d, %Y")
+            time = now.strftime("%H:%M")
 
-        # market open/close
-        m_open = float( soup.find_all("td", {"class":"Ta(end) Fw(600) Lh(14px)"})[1].find('span').text.replace(',', '') )
-        p_close = float( soup.find_all("td", {"class":"Ta(end) Fw(600) Lh(14px)"})[0].find('span').text.replace(',', '') )
+            # price
+            price = float( soup.find("div", {'class':"My(6px) Pos(r) smartphone_Mt(6px)"}).find('span').text.replace(',', '') )
 
-        # volume
-        volume = float( soup.find_all("td", {"class":"Ta(end) Fw(600) Lh(14px)"})[6].find('span').text.replace(',', '') )
+            # Moving Averages
+            fifty_day_SMA = float( stats_soup.find_all("tr" , {"class":"Bxz(bb) H(36px) BdB Bdbc($seperatorColor)"})[4].find("td", {"class": "Fz(s) Fw(500) Ta(end) Pstart(10px) Miw(60px)"}).text.replace(",", '') )
+            t_hundred_day_SMA = float( stats_soup.find_all("tr" , {"class":"Bxz(bb) H(36px) BdB Bdbc($seperatorColor)"})[5].find("td", {"class": "Fz(s) Fw(500) Ta(end) Pstart(10px) Miw(60px)"}).text.replace(",", '') )
 
-        # sentiment
-        analyze = sentiment.Analysis(ticker);
-        sent, subj = analyze.run()
+            # market open/close
+            m_open = float( soup.find_all("td", {"class":"Ta(end) Fw(600) Lh(14px)"})[1].find('span').text.replace(',', '') )
+            p_close = float( soup.find_all("td", {"class":"Ta(end) Fw(600) Lh(14px)"})[0].find('span').text.replace(',', '') )
 
-        # append info
-        entry = [date, time, price, fifty_day_SMA, t_hundred_day_SMA, m_open, p_close, volume, sent, subj]
-        stock_dict[ticker] = stock_dict[ticker].append(pd.Series(entry, index=columns), ignore_index=True)
+            # volume
+            volume = float( soup.find_all("td", {"class":"Ta(end) Fw(600) Lh(14px)"})[6].find('span').text.replace(',', '') )
+
+            # sentiment
+            analyze = sentiment.Analysis(ticker);
+            sent, subj = analyze.run()
+
+            # append info
+            entry = [date, time, price, fifty_day_SMA, t_hundred_day_SMA, m_open, p_close, volume, float(sent), float(subj)]
+            stock_dict[ticker] = stock_dict[ticker].append(pd.Series(entry, index=columns), ignore_index=True)
+            t.sleep(60)
     manage_data.manage_csv(dictionary)
 
 def display_information(stocks = stocks_list, dictionary = stock_dict):
