1be91dd03c06947a39bfa6824f2b579fc27f392f
jcr678 <51676314+jcr678@users.noreply.github.com>
2020-01-02 20:53:21 -0600

    planning out main

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import sentiment
 def main():
     scrape.scrape()
     # scrape.display_information()
-
+def test_actor_critic():
+    pass
+def test_q_learning():
+    pass
 if __name__ == "__main__":
     main()
+    test_actor_critic()
+    test_q_learning()
