0d7bf9fbfb1d178d6430bb55ed3c24c5b31df9ae
jcr678 <51676314+jcr678@users.noreply.github.com>
2020-01-04 17:52:22 -0600

    env is done

--- env.py
+++ env.py
@@ -20,47 +20,47 @@ INITIAL_ACCOUNT_BALANCE = 10000.0
 
 
 class CustomEnv(gym.Env):
-  __init__(self, dataFrame_list, stocks_list, isATest): # order of df list and stocks list MUST be same
-    super(CustomEnv, self).__init__()
+    def __init__(self, dataFrame_list, stocks_list, isATest): # order of df list and stocks list MUST be same
+        super(CustomEnv, self).__init__()
 
-    main_df = pd.DataFrame()
-    for stock in stocks_list: # Join all stocks df into single df
-      dataFrame_list[stock].rename(columns={'Price': f"{stock}_Price",'50-Day MA': f"{stock}_50-Day MA",\\
-                                            '200-Day MA': f"{stock}_200-Day MA",\\
-                                            'Market Open': f"{stock}_Market Open",'Prev Close': f"{stock}_Prev Close",\\
-                                            'Trading Volume': f"{stock}_Trading Volume",'Sentiment': f"{stock}_Sentiment",\\
+        main_df = pd.DataFrame()
+        for stock in stocks_list: # Join all stocks df into single df
+            dataFrame_list[stock].rename(columns={'Price': f"{stock}_Price",'50-Day MA': f"{stock}_50-Day MA", \
+                                            '200-Day MA': f"{stock}_200-Day MA",\
+                                            'Market Open': f"{stock}_Market Open",'Prev Close': f"{stock}_Prev Close",\
+                                            'Trading Volume': f"{stock}_Trading Volume",'Sentiment': f"{stock}_Sentiment",\
                                             'Subjectivity': f"{stock}_Subjectivity",}, inplace=True)
-      if len(main_df)==0:  # If dataframe is empty
+        if len(main_df)==0:  # If dataframe is empty
           main_df = dataFrame_list[stock]  # then it's just the current df
           n_columns = dataFrame_list[stock].columns - 2 # dont include date/time
-      else:  # otherwise, join this data to the main one
+        else:  # otherwise, join this data to the main one
           dataFrame_list[stock].drop(columns=['Date', 'Time']) # Delete the date/time if it is not the first stock b/c repeats
           main_df = main_df.join(dataFrame_list[stock])
-          
-    main_df.dropna(inplace=True) #drop nan values
-    
-    self.stocks_list = stocks_list
-    self.reward_range = (0, MAX_ACCOUNT_BALANCE)
-    self.n_stocks = len(stocks_list)
-    self.main_df = normalizeBetweenZeroAndOne(main_df)
-    self.cur_step = 0
-    self.n_observes = 2*60*24 # not including current observe (must add one)
-    self.OHLC_ect = columns  # Open high low close, sentiment ect...
-    self.shared_vals = 2 # balance, net worth
-    self.unshared_vals = 4 # shares held, cost basis, ect...
-    self.isATest = isATest # Boolean: call alpaca or do not call alpaca
-    
-    # Action space: discrete # of action types (buy, sell, and hold) &
-    # continuous spectrum of amounts to buy/sell (0-100% of balance/n_stocks or shares_held/n_stocks).
-    # Actions are 0,1,2 and percent/n for n stocks
-    self.action_space = spaces.Box(low=0, high=1, shape=(self.n_stocks*2,) dtype=np.float16)
-    # Observations are ohlc ect as percentages for n observations and n stocks
-    self.observation_space = spaces.Box(low=0, high=1, shape=(self.n_observes+1, 
+
+        main_df.dropna(inplace=True) #drop nan values
+
+        self.stocks_list = stocks_list
+        self.reward_range = (0, MAX_ACCOUNT_BALANCE)
+        self.n_stocks = len(stocks_list)
+        self.main_df = normalizeBetweenZeroAndOne(main_df)
+        self.cur_step = 0
+        self.n_observes = 2*60*24 # not including current observe (must add one)
+        self.OHLC_ect = columns  # Open high low close, sentiment ect...
+        self.shared_vals = 2 # balance, net worth
+        self.unshared_vals = 4 # shares held, cost basis, ect...
+        self.isATest = isATest # Boolean: call alpaca or do not call alpaca
+
+        # Action space: discrete # of action types (buy, sell, and hold) &
+        # continuous spectrum of amounts to buy/sell (0-100% of balance/n_stocks or shares_held/n_stocks).
+        # Actions are 0,1,2 and percent/n for n stocks
+        self.action_space = spaces.Box(low=0, high=1, shape=(self.n_stocks*2,), dtype=np.float16)
+        # Observations are ohlc ect as percentages for n observations and n stocks
+        self.observation_space = spaces.Box(low=0, high=1, shape=(self.n_observes+1, 
                                                               self.shared_vals+(self.n_columns+self.unshared_vals)*self.n_stocks),
                                         dtype=np.float16) # Need to change shape because basic values are diff for diff stocks
-  def _next_observation(self):
-    '''
-    for stock in self.stocks_list:shape=(((self.n_observes+1)*self.OHLC_ect*self.n_stocks+self.basic_values,)
+    def _next_observation(self):
+        '''
+        for stock in self.stocks_list:shape=(((self.n_observes+1)*self.OHLC_ect*self.n_stocks+self.basic_values,)
         f = np.array([[ #assuming normalized between 0 and 1
                     df.loc[self.current_step - self.n_observes: self.current_step,
                            f"{stock}_Date"].values,
@@ -76,7 +76,7 @@ class CustomEnv(gym.Env):
                 ]])
         frame = np.append(frame, f)
                     # Append additional data and scale each value to between 0-1
-    obs = np.append(frame, [ # has length of basic_values
+        obs = np.append(frame, [ # has length of basic_values
                 balance / MAX_ACCOUNT_BALANCE,
                 max_net_worth / MAX_ACCOUNT_BALANCE,
                 shares_held / MAX_NUM_SHARES, # Need to update all use list
@@ -84,25 +84,25 @@ class CustomEnv(gym.Env):
                 total_shares_sold / MAX_NUM_SHARES, # Need to update all use list
                 total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE), # ?Need to update all use list?
             ], axis=0)
-    '''
-    obs = []
-    for i in range(self.current_step - self.n_observes, self.current_step+1):
-        # get given row as list
-        row = self.main_df.iloc[[i]].tolist()
-        # data shared by all stocks)
-        row.append(balance / MAX_ACCOUNT_BALANCE)
-        row.append(max_net_worth / MAX_ACCOUNT_BALANCE)
-        # data tied to a given stock and not shared by all stocks
-        for i, stock in self.stocks_list:
-            row.append(self.shares_held[i] / MAX_NUM_SHARES)
-            row.append(cost_basis[i] / MAX_SHARE_PRICE)
-            row.append(total_shares_sold[i] / MAX_NUM_SHARES)
-            row.append(total_sales_value[i] / (MAX_NUM_SHARES * MAX_SHARE_PRICE))
-        #append the row to the obs
-        obs.append(row)
-    return obs
+        '''
+        obs = []
+        for i in range(self.current_step - self.n_observes, self.current_step+1):
+            # get given row as list
+            row = self.main_df.iloc[[i]].tolist()
+            # data shared by all stocks)
+            row.append(balance / MAX_ACCOUNT_BALANCE)
+            row.append(max_net_worth / MAX_ACCOUNT_BALANCE)
+            # data tied to a given stock and not shared by all stocks
+            for i, stock in self.stocks_list:
+                row.append(self.shares_held[i] / MAX_NUM_SHARES)
+                row.append(cost_basis[i] / MAX_SHARE_PRICE)
+                row.append(total_shares_sold[i] / MAX_NUM_SHARES)
+                row.append(total_sales_value[i] / (MAX_NUM_SHARES * MAX_SHARE_PRICE))
+                #append the row to the obs
+            obs.append(row)
+        return obs
 
-  def normalizeBetweenZeroAndOne(main_df):
+    def normalizeBetweenZeroAndOne(main_df):
         # update date
         dates = main_df['Date'].toList()
         main_df['Date'] = [float(day.strip())/MAX_TIME for day in dates] # get dates w/out spaces as floats; divided by max date
@@ -120,40 +120,46 @@ class CustomEnv(gym.Env):
             main_df[stock + "_" + 'Trading Volume'] = [v/MAX_VOLUME for v in volList]
         #other columns already between 0 and 1
         return main_df
-  def _take_action(self, action): # Buy Sell ect..
+        def _take_action(self, action): # Buy Sell ect..
         #use self.isATest to know if u actually buy stocks
-        for i, stock in enumerate(self.stocks_list): # take actions
-          if action[i*2] < 1.0/3.0: # buy
-           buy(self, action[i*2+1], stock, i)
-          else if action[i*2] > 2.0/3.0: # sell
-            sell(self, action[i*2+1], stock, i)
-          #hold (do nothing so no statement)
-      
-  def buy(self, n_percent, stock, i):
+            for i, stock in enumerate(self.stocks_list):
+                if action[i*2] < 1.0/3.0: # buy
+                    buy(self, action[i*2+1], stock, i)
+                elif action[i*2] > 2.0/3.0: # sell
+                    sell(self, action[i*2+1], stock, i)
+                    #hold (do nothing so no statement)
+
+    def buy(self, n_percent, stock, i):
         price = main_df.at(self.step, f"{stock}_Price")
         if self.isATest: #dont use alpaca
-          # Buy amount % of balance in shares
-          total_possible = int(self.balance / (price * self.n_stocks))
-          shares_bought = int(total_possible * n_percent)
-          prev_cost = self.cost_basis[i] * self.shares_held[i] # need to turn shares held and cost basis arrays
-          additional_cost = shares_bought * price
+            # Buy amount % of balance in shares
+            total_possible = int(self.balance / (price * self.n_stocks))
+            shares_bought = int(total_possible * n_percent)
+            prev_cost = self.cost_basis[i] * self.shares_held[i] # need to turn shares held and cost basis arrays
+            additional_cost = shares_bought * price
 
-          self.balance -= additional_cost
-          self.cost_basis[i] = (
+            self.balance -= additional_cost
+            self.cost_basis[i] = (
                 prev_cost + additional_cost) / (self.shares_held[i] + shares_bought)
-          self.shares_held[i] += shares_bought
+            self.shares_held[i] += shares_bought
 
         else: #use alpaca
-          
-    
-  def sell(self, n_percent, stock, i):
+            blah = "blah"
+            
+    def sell(self, n_percent, stock, i):
         price = main_df.at(self.step, f"{stock}_Price")
         if self.isATest:
-          #dont use alpaca
+            # Sell amount % of shares held
+            shares_sold = int(self.shares_held[i] * n_percent/self.n_stocks)
+            self.balance += shares_sold * current_price
+            self.shares_held[i] -= shares_sold
+            self.total_shares_sold[i] += shares_sold
+            self.total_sales_value[i] += shares_sold * current_price
         else:
-          #use alpaca
-          
-  def _step(self, action):
+            #use alpaca
+            blah = "blah"
+
+    def _step(self, action):
         # Execute one time step within the environment
         self._take_action(action) 
 
@@ -163,28 +169,41 @@ class CustomEnv(gym.Env):
             self.current_step = 0
 
         delay_modifier = (self.current_step / MAX_STEPS)
-        
+
         reward = self.balance * delay_modifier
         done = self.net_worth <= 0
 
         obs = self._next_observation()
 
         return obs, reward, done, {}
-   def reset(self):# Reset the state of the environment to an initial state
-    # Reset the state of the environment to an initial state
+
+    def reset(self):# Reset the state of the environment to an initial state
+        # Reset the state of the environment to an initial state
         self.balance = INITIAL_ACCOUNT_BALANCE
         self.net_worth = INITIAL_ACCOUNT_BALANCE
         self.max_net_worth = INITIAL_ACCOUNT_BALANCE
         self.shares_held = [0] * self.n_stocks
         self.cost_basis = [0] * self.n_stocks
         self.total_shares_sold = [0] * self.n_stocks
-        self.total_sales_value = [0] * self.n_stocks # ???? idk ill figure out when i do sell
+        self.total_sales_value = [0] * self.n_stocks 
 
         # Set the current step to a random point within the data frame
         self.current_step = random.randint(
             0, len(main_df.index) - 1)
 
         return self._next_observation()
-        # Do other stuff
+
     def render(self, mode='human', close=False):# Print stuff to console
-        pass
+        
+        # Render the environment to the screen
+        profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
+        print(f'Step: {self.current_step}')
+        print(f'Balance: {self.balance}')
+        
+        for i, stock in enumerate(stocks_list):
+            print(stock)
+            print(f'shares held: {self.shares_held[i]} (Total sold: {self.total_shares_sold[i]})')
+            print(f'Avg cost for held shares: {self.cost_basis[i]} (Total sales value: {self.total_sales_value[i]})')
+            
+        print(f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
+        print(f'Profit: {profit}')
